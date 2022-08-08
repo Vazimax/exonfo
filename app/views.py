6f0c7b1f26b7cc83c73388a2ba90b985d3224a69
Vazimax <aboubakr.elhabti@gmail.com>
2022-08-08 20:12:35 +0100

    minimising the copied code

--- app/views.py
+++ app/views.py
@@ -26,18 +26,24 @@ def pc_general(request):
 
 def pc_mechanics(request):
 
-    mechanic_words = PCWord.objects.all().filter(field='Mechanics')
+    general_words = PCWord.objects.all().filter(field='General')
+    filter = OrderFilter(request.GET,queryset=general_words)
+    general_words = filter.qs
     context = {
-        'mechanic_words':mechanic_words
+        'general_words':general_words,
+        'filter':filter,
     }
 
-    return render(request,'pc/mechanics.html',context)
+    return render(request,'pc/pc_general.html',context)
 
 def pc_electricity(request):
 
     electricity_words = PCWord.objects.all().filter(field='Electricity')
+    filter = OrderFilter(request.GET,queryset=electricity_words)
+    electricity_words = filter.qs
     context = {
-        'electricity_words':electricity_words
+        'electricity_words':electricity_words,
+        'filter':filter,
     }
 
     return render(request,'pc/electricity.html',context)
@@ -45,8 +51,11 @@ def pc_electricity(request):
 def pc_optics(request):
 
     optics_words = PCWord.objects.all().filter(field='Optics')
+    filter = OrderFilter(request.GET,queryset=optics_words)
+    optics_words = filter.qs
     context = {
-        'optics_words':optics_words
+        'optics_words':optics_words,
+        'filter':filter,
     }
 
     return render(request,'pc/optics.html',context)
@@ -54,17 +63,23 @@ def pc_optics(request):
 def pc_waves(request):
 
     waves_words = PCWord.objects.all().filter(field='Waves')
+    filter = OrderFilter(request.GET,queryset=waves_words)
+    waves_words = filter.qs
     context = {
-        'waves_words':waves_words
+        'waves_words':waves_words,
+        'filter':filter,
     }
 
-    return render(request,'pc/waves.html',context)
+    return render(request,'pc/general.html',context)
 
 def pc_nuclear(request):
 
     nuclear_words = PCWord.objects.all().filter(field='Nuclear transformation')
+    filter = OrderFilter(request.GET,queryset=nuclear_words)
+    nuclear_words = filter.qs
     context = {
-        'nuclear_words':nuclear_words
+        'nuclear_words':nuclear_words,
+        'filter':filter,
     }
 
     return render(request,'pc/nuclear.html',context)
@@ -72,8 +87,11 @@ def pc_nuclear(request):
 def pc_matter(request):
 
     matter_words = PCWord.objects.all().filter(field='Matter')
+    filter = OrderFilter(request.GET,queryset=matter_words)
+    matter_words = filter.qs
     context = {
-        'matter_words':matter_words
+        'matter_words':matter_words,
+        'filter':filter,
     }
 
     return render(request,'pc/matter.html',context)
